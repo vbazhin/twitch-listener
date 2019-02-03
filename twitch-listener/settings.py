945fa98118e8eb224599a29082e372c451e4cd0d
vbazhin <v.bazhin@gmail.com>
2019-02-03 21:51:20 +1000

    Credentials replaced with mocks in settings.py

--- twitch-listener/settings.py
+++ twitch-listener/settings.py
@@ -2,13 +2,11 @@ DEBUG = True
 
 API_BASE_URL = 'https://api.twitch.tv/helix/'
 
-if DEBUG:
-    AUTH_REDIRECT_URI = 'http://127.0.0.1:5000/auth'
-else:
-    AUTH_REDIRECT_URL = '206.189.173.10:5000/auth'
 
-CLIENT_ID = 'vvg1sdkexkza9ctnnqs9v12re2qxkl'
+AUTH_REDIRECT_URI = '<your_server_address>/auth'
 
-CLIENT_SECRET = 'wruq66igz3sqnb5kneim8kk0u5y8xo'
+CLIENT_ID = '<client_id_of_your_app>'
 
-CALLBACK_URL = 'http://206.189.173.10:5000/callback'
+CLIENT_SECRET = '<client_secret_of_your_app'
+
+CALLBACK_URL = '<your_server_address>'
