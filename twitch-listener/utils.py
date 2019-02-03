a0d12be657112d0524c84e910874c356546a359a
vbazhin <v.bazhin@gmail.com>
2019-02-04 00:18:49 +1000

    Fix utils.join_urls()

--- twitch-listener/utils.py
+++ twitch-listener/utils.py
@@ -1,4 +1,4 @@
 """Utilities for Twitch Listener needs."""
 
 def join_urls(*args):
-    return '/'.join(arg.strip('/') for arg in args)
+    return '/'.join(arg.strip('/') for arg in args).strip('/')
